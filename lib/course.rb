1312899c04a0872796e9b5ae22eb4d2d9112d371
Reid Miller <reid.s.miller@gmail.com>
2023-02-15 09:33:23 -0700

    feat: Add course class and methods

--- lib/course.rb
+++ lib/course.rb
@@ -1,1 +1,16 @@
+class Course
+  attr_reader :name, :capacity, :students
+  def initialize (name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+  end
 
+  def enroll(student)
+    @students << student if self.full? == false
+  end
+
+  def full?
+    @students.length == @capacity
+  end
+end
