9365256050d9bf7a3123c2093af837db35deaeb5
Reid Miller <reid.s.miller@gmail.com>
2023-02-15 10:52:40 -0700

    test: Add tests for iteration 4

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Gradebook do
       course2.enroll(student3)
       course2.enroll(student4)
 
-      expect(gradebook.list_all_students).to eq([student1, student2, student3, student4])
+      expect(gradebook.list_all_students).to eq({"Calculus"=>[student1, student2,], "Algebra"=>[student3, student4]})
     end
 
     it 'can show students below threshold' do
@@ -66,7 +66,65 @@ RSpec.describe Gradebook do
       student4.log_score(90)
       student4.log_score(49)
 
-      expect(gradebook.students_below(70.0)).to eq([student2, student4])
+      expect(gradebook.students_below(70)).to eq([student2, student4])
+    end
+  end
+
+  describe '#Iteration 4' do
+    it 'can track all grades across all courses' do
+      gradebook = Gradebook.new('Prof Einstein')
+      course1 = Course.new('Calculus', 2)
+      course2 = Course.new('Algebra', 3)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      student3 = Student.new({name: "Bob", age: 19})
+      student4 = Student.new({name: "Fela", age: 27})
+      
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+
+      course1.enroll(student1)
+      course1.enroll(student2)
+      course2.enroll(student3)
+      course2.enroll(student4)
+
+      student1.log_score(88)
+      student1.log_score(90)
+      student2.log_score(50)
+      student2.log_score(65)
+      student3.log_score(70)
+      student4.log_score(90)
+      student4.log_score(49)
+
+      expect(gradebook.all_grades).to eq({"Morgan"=>[89.0], "Jordan"=>[57.5], "Bob"=>[70.0], "Fela"=>[69.5]})
+    end
+
+    it 'can find students within a given grade range' do
+      gradebook = Gradebook.new('Prof Einstein')
+      course1 = Course.new('Calculus', 2)
+      course2 = Course.new('Algebra', 3)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      student3 = Student.new({name: "Bob", age: 19})
+      student4 = Student.new({name: "Fela", age: 27})
+      
+      gradebook.add_course(course1)
+      gradebook.add_course(course2)
+
+      course1.enroll(student1)
+      course1.enroll(student2)
+      course2.enroll(student3)
+      course2.enroll(student4)
+
+      student1.log_score(88)
+      student1.log_score(90)
+      student2.log_score(50)
+      student2.log_score(65)
+      student3.log_score(70)
+      student4.log_score(90)
+      student4.log_score(49)
+
+      expect(gradebook.students_grade_range(70, 90)).to eq([student1, student3])
     end
   end
 end
